2f4522fa62d97f312f6aa93bff87399d0fecfb63
Äkwav <xekwavx@gmail.com>
2021-09-07 21:11:39 +0200

    allow null

--- Socket/SkyblockBackEnd.cs
+++ Socket/SkyblockBackEnd.cs
@@ -345,7 +345,7 @@ namespace hypixel
 
         public bool SendFlip(FlipInstance flip)
         {
-            if (!Settings.MatchesSettings(flip))
+            if (!(Settings?.MatchesSettings(flip) ?? false))
                 return true;
             var data = new MessageData("flip", JSON.Stringify(flip), 60);
             return TrySendData(data);
