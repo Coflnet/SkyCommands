2bcbcac2bffc24fc71b984861905c56c8e8d54e3
Matthias Luger <malua007@live.de>
2021-11-06 04:26:59 +0100

    click command

--- Commands/Minecraft/ClickedCommand.cs
+++ Commands/Minecraft/ClickedCommand.cs
@@ -1,12 +1,49 @@
 using System.Threading.Tasks;
+using hypixel;
+using MessagePack;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Coflnet.Sky.Commands.MC
 {
     public class ClickedCommand : McCommand
     {
-        public override Task Execute(MinecraftSocket socket, string arguments)
+        public async override Task Execute(MinecraftSocket socket, string arguments)
         {
-            return Task.CompletedTask;
+
+            var command = JsonConvert.DeserializeObject<string>(arguments);
+
+            if (command.Split(" ")[0] != "/viewauction")
+            {
+                return;
+            }
+
+            var auctionUUID = command.Split(" ")[1];
+
+            //var affected = SubscribeEngine.Instance.Unsubscribe(userId, args.Topic,args.Type).Result;
+            var request = new RestRequest("Tracker/flipEvent/{auctionUUID}", Method.POST)
+                .AddJsonBody(new Arguments() { FlipTrackerEvent = FlipEventType.FLIP_CLICK, PlayerUUID = socket.McId })
+                .AddUrlSegment("auctionUUID", auctionUUID);
+            var response = await TrackerClient.Client.ExecuteAsync(request);
+        }
+
+        [MessagePackObject]
+        public class Arguments
+        {
+            [Key("playerUUID")]
+            public string PlayerUUID;
+            [Key("flipEventType")]
+            public FlipEventType FlipTrackerEvent;
+
+        }
+
+        public enum FlipEventType
+        {
+            FLIP_RECEIVE = 1,
+            FLIP_CLICK = 2,
+            PURCHASE_START = 4,
+            PURCHASE_CONFIRM = 8,
+            AUCTION_SOLD = 16
         }
     }
 }
