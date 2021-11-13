33d8feaaff8dda9459c3ab30e81512b3288131c6
Äkwav <xekwavx@gmail.com>
2021-11-14 00:03:22 +0100

    remove all non-unlocked crafts

--- Controllers/CraftingController.cs
+++ Controllers/CraftingController.cs
@@ -19,19 +19,41 @@ namespace Coflnet.Hypixel.Controller
     public class CraftingController : ControllerBase
     {
         private static RestClient client = null;
+        private static RestClient profileClient = null;
         public CraftingController(IConfiguration config)
         {
             if (client == null)
                 client = new RestClient("http://" + config["CRAFTS_HOST"]);
+            if (profileClient == null)
+                profileClient = new RestClient("http://" + config["PROFILE_HOST"]);
         }
 
         [Route("profit")]
         [HttpGet]
-        public async Task<IEnumerable<ProfitableCraft>> GetProfitable(string profile = null)
+        public async Task<IEnumerable<ProfitableCraft>> GetProfitable(string player = null, string profile = null)
         {
             var response = await client.ExecuteAsync(new RestRequest("Crafts/profit").AddParameter("profile", profile));
             var crafts = JsonConvert.DeserializeObject<List<ProfitableCraft>>(response.Content);
-            return crafts;
+            if (profile == null)
+                return crafts;
+            var collectionJson = await profileClient.ExecuteAsync(new RestRequest($"/api/profile/{player}/{profile}/data/collections"));
+            var collection = JsonConvert.DeserializeObject<Dictionary<string, CollectionElem>>(collectionJson.Content);
+            var list = new List<ProfitableCraft>();
+            foreach (var item in crafts)
+            {
+                if(item.ReqCollection == null 
+                || collection.TryGetValue(item.ReqCollection.Name, out CollectionElem elem) 
+                        && elem.tier >= item.ReqCollection.Level)
+                    list.Add(item);
+                else 
+                    Console.WriteLine("Blocked " + item.ItemId + " " + item.ReqCollection.Name);
+            }
+            return list;
         }
+    }
+
+    public class CollectionElem
+    {
+        public int tier;
     }
 }
