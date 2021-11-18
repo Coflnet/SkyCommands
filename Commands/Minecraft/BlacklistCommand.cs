7eaa37a245545f5ce9ac25faa427c3a871210dab
Äkwav <xekwavx@gmail.com>
2021-11-18 07:50:02 +0100

    create blacklist if non existent

--- Commands/Minecraft/BlacklistCommand.cs
+++ Commands/Minecraft/BlacklistCommand.cs
@@ -7,6 +7,8 @@ namespace Coflnet.Sky.Commands.MC
         public override Task Execute(MinecraftSocket socket, string arguments)
         {
             var tag = Newtonsoft.Json.JsonConvert.DeserializeObject<string>(arguments);
+            if(socket.Settings.BlackList == null)
+                socket.Settings.BlackList = new System.Collections.Generic.List<Filter.ListEntry>();
             socket.Settings.BlackList.Add(new Filter.ListEntry(){ItemTag = tag});
             socket.SendMessage(COFLNET + $"You blacklisted all {arguments} from appearing");
             return Task.CompletedTask;
