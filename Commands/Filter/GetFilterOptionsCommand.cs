050d845236106fbb9ee031c196cfb70b5165cd86
Äkwav <xekwavx@gmail.com>
2022-02-12 18:04:43 +0100

    document filter options command

--- Commands/Filter/GetFilterOptionsCommand.cs
+++ Commands/Filter/GetFilterOptionsCommand.cs
@@ -5,14 +5,16 @@ using hypixel;
 
 namespace Coflnet.Sky.Commands
 {
+    /// <summary>
+    /// List options for filters
+    /// </summary>
     public class GetFilterOptionsCommand : Command
     {
-
         static FilterEngine fe = new FilterEngine();
         public override Task Execute(MessageData data)
         {
             var filter = fe.GetFilter(data.GetAs<string>());
-            return data.SendBack(data.Create("filterOptions",new FilterOptions(filter),A_DAY/2));
+            return data.SendBack(data.Create("filterOptions", new FilterOptions(filter), A_DAY / 2));
         }
     }
 }
