195ea24a14480e1c127741fdd7a6411b46a3184d
Äkwav <xekwavx@gmail.com>
2022-03-24 23:00:26 +0100

    refage update

--- Commands/Search/SearchCommand.cs
+++ Commands/Search/SearchCommand.cs
@@ -13,8 +13,8 @@ namespace hypixel
             Regex rgx = new Regex("[^a-zA-Z0-9_]");
             var search = rgx.Replace(data.Data, "").ToLower();
 
-            var players = PlayerSearch.Instance.Search(search,5);
-            return data.SendBack(data.Create("searchResponse", players,A_WEEK));
+            var players = PlayerSearch.Instance.Search(search, 5);
+            return data.SendBack(data.Create("searchResponse", players, A_WEEK));
         }
     }
 }
