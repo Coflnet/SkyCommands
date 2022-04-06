bdd0955230cb6ea4de271b1cd54e53f76c88adae
Äkwav <xekwavx@gmail.com>
2022-04-06 20:57:57 +0200

    check premium from payments service

--- Commands/Premium/HasPremiumCommand.cs
+++ Commands/Premium/HasPremiumCommand.cs
@@ -2,13 +2,16 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Payments.Client.Api;
+using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Coflnet.Sky.Commands
 {
     public class PremiumExpirationCommand : Command
     {
         static string premiumPlanName = SimplerConfig.SConfig.Instance["PRODUCTS:PREMIUM"];
+
         public override async Task Execute(MessageData data)
         {
             try
@@ -22,13 +25,19 @@ namespace Coflnet.Sky.Commands
                         return;
                     }
                 }
-
-                var api = new UserApi();
-                var until = await api.UserUserIdOwnsProductSlugUntilGetAsync(data.UserId.ToString(), premiumPlanName);
-                if (until > DateTime.Now)
+                try
                 {
-                    await data.SendBack(data.Create("premiumExpiration", until));
-                    return;
+                    var api  = DiHandler.ServiceProvider.GetService<UserApi>();
+                    var until = await api.UserUserIdOwnsProductSlugUntilGetAsync(data.UserId.ToString(), premiumPlanName);
+                    if (until > DateTime.Now)
+                    {
+                        await data.SendBack(data.Create("premiumExpiration", until));
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    dev.Logger.Instance.Error(e, "checking premium");
                 }
             }
             catch (Exception)
