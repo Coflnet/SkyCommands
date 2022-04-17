d5919aca4183b9894c8152f2733d439dc55ba1f8
Äkwav <xekwavx@gmail.com>
2022-04-17 14:54:27 +0200

    return value if no valid uuid

--- Commands/PlayerDetails/PlayerNameCommand.cs
+++ Commands/PlayerDetails/PlayerNameCommand.cs
@@ -14,6 +14,8 @@ namespace Coflnet.Sky.Commands
 
         public static async Task<MessageData> CreateResponse(MessageData data, string uuid)
         {
+            if(uuid.Length < 32)
+                return data.Create("nameResponse", uuid, 60);
             var name = await PlayerSearch.Instance.GetName(uuid);
             // player names don't change often, but are easy to compute
             return data.Create("nameResponse", name, 0);
