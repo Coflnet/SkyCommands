b9a25ebd60beb080b09bcc1a63100d5246cf2989
Äkwav <xekwavx@gmail.com>
2022-04-19 16:16:37 +0200

    mitigate fast settings changes

--- Commands/Flipper/FlipSettingsSetCommand.cs
+++ Commands/Flipper/FlipSettingsSetCommand.cs
@@ -4,26 +4,40 @@ using Coflnet.Sky.Commands.Shared;
 using Microsoft.Extensions.DependencyInjection;
 using Coflnet.Sky.Core;
 using System.Runtime.Serialization;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Coflnet.Sky.Commands
 {
     public class FlipSettingsSetCommand : Command
     {
         private static SettingsUpdater updater = new SettingsUpdater();
+        private ConcurrentDictionary<int, SemaphoreSlim> Locks = new();
+
         public override async Task Execute(MessageData data)
         {
             var arguments = data.GetAs<Update>();
             var service = DiHandler.ServiceProvider.GetRequiredService<SettingsService>();
-            if(string.IsNullOrEmpty(arguments.Key))
+            if (string.IsNullOrEmpty(arguments.Key))
                 throw new CoflnetException("missing_key", "available options are:\n" + String.Join(",\n", updater.Options()));
             var value = arguments.Value.Replace('$', '§');
             var socket = (data as SocketMessageData).Connection;
-            if(socket.FlipSettings == null)
-                socket.FlipSettings = await SelfUpdatingValue<FlipSettings>.Create(data.UserId.ToString(), "flipSettings");
-            await updater.Update(socket, arguments.Key, value);
-            socket.Settings.Changer = arguments.Changer;
-            data.Log(Newtonsoft.Json.JsonConvert.SerializeObject(socket.Settings,Newtonsoft.Json.Formatting.Indented));
-            await service.UpdateSetting(data.UserId.ToString(), "flipSettings", socket.Settings);
+
+            var lazyLock = Locks.GetOrAdd(data.UserId, id => new SemaphoreSlim(1));
+            try
+            {
+                await lazyLock.WaitAsync();
+                if (socket.FlipSettings == null)
+                    socket.FlipSettings = await SelfUpdatingValue<FlipSettings>.Create(data.UserId.ToString(), "flipSettings");
+                await updater.Update(socket, arguments.Key, value);
+                socket.Settings.Changer = arguments.Changer;
+                data.Log(Newtonsoft.Json.JsonConvert.SerializeObject(socket.Settings, Newtonsoft.Json.Formatting.Indented));
+                await service.UpdateSetting(data.UserId.ToString(), "flipSettings", socket.Settings);
+            }
+            finally
+            {
+                lazyLock.Release();
+            }
         }
 
         [DataContract]
