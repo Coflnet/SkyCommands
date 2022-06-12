22d89e137346c6d23d02b1c54e15c49225036ddd
Äkwav <ekwav@coflnet.com>
2022-06-12 13:12:00 +0200

    display error message for non connected users

--- Commands/Premium/TransferCoinsCommand.cs
+++ Commands/Premium/TransferCoinsCommand.cs
@@ -16,7 +16,12 @@ namespace Coflnet.Sky.Commands
             if(!string.IsNullOrEmpty(args.TargetUserEmail))
                 targetUser = (await UserService.Instance.GetUserIdByEmail(args.TargetUserEmail)).ToString();
             else if(!string.IsNullOrEmpty(args.TargetUserMc) && args.TargetUserMc.Length == 32)
-                targetUser = (await McAccountService.Instance.GetUserId(args.TargetUserMc)).ExternalId;
+            {
+                var userInfo = await McAccountService.Instance.GetUserId(args.TargetUserMc);
+                if(userInfo == null)
+                    throw new CoflnetException("not_found", "That user doesn't have any verified accounts. Please tell them to connect their Minecraft account to their Coflnet account or ask them for their email");
+                targetUser = userInfo.ExternalId;
+            }
             else 
                 throw new CoflnetException("missing_argument","Either `email` or `mcId` have to be passed to know where to send funds to");
             if(targetUser == "0")
