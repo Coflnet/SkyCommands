d6a54ca8cea064c065270fbc951f2c8d4bdbe883
Äkwav <ekwav@coflnet.com>
2022-08-22 19:35:00 +0200

    stop balance from caching

--- Commands/Premium/GetCoflBalanceCommand.cs
+++ Commands/Premium/GetCoflBalanceCommand.cs
@@ -6,11 +6,12 @@ namespace Coflnet.Sky.Commands
 {
     public class GetCoflBalanceCommand : Command
     {
+        public override bool Cacheable => false;
         public override async Task Execute(MessageData data)
         {
             var productsApi = new UserApi("http://" + SimplerConfig.Config.Instance["PAYMENTS_HOST"]);
             var userData = await productsApi.UserUserIdGetAsync(data.UserId.ToString());
-            await data.SendBack(data.Create("coflBalance", userData.Balance, A_MINUTE / 6));
+            await data.SendBack(data.Create("coflBalance", userData.Balance));
         }
     }
 
