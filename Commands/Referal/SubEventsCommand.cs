14b418a85d574dbdd9f92f10c4999d1174addff7
Äkwav <ekwav@coflnet.com>
2022-09-06 18:31:03 +0200

    g when events

--- Commands/Referal/SubEventsCommand.cs
+++ Commands/Referal/SubEventsCommand.cs
@@ -9,12 +9,14 @@ namespace Coflnet.Sky.Commands
     {
         public override Task Execute(MessageData data)
         {
-            if(!(data is SocketMessageData socketData))
+            if (!(data is SocketMessageData socketData))
                 throw new CoflnetException("invalid_protocol", "This is a socket command");
             var broker = DiHandler.ServiceProvider.GetRequiredService<EventBrokerClient>();
             var id = data.mId;
+            System.Console.WriteLine($"{data.UserId} subbed to events");
             var sub = broker.SubEvents(data.UserId.ToString(), ev =>
             {
+                System.Console.WriteLine($"{data.UserId} receives event {ev.SourceType} {ev.Message}");
                 var response = data.Create("event", ev);
                 response.mId = id;
                 data.SendBack(response);
