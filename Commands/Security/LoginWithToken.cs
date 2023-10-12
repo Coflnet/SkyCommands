4b7417bde9ff827e1acc67b473e2f6131e784e3c
Äkwav <ekwav@coflnet.com>
2023-10-12 18:31:41 +0200

    fix #47

--- Commands/Security/LoginWithToken.cs
+++ Commands/Security/LoginWithToken.cs
@@ -7,11 +7,24 @@ public class LoginWithToken : Command
 {
     public override async Task Execute(MessageData data)
     {
+        try
+        {
+            if (data is not SocketMessageData con)
+                return;
+            await LoginWithSelfToken(data, con);
+        }
+        catch (System.Exception e)
+        {
+            dev.Logger.Instance.Error(e, "login with token default");
+            await new SetGoogleIdCommand().Execute(data);
+        }
+    }
+
+    private static async Task LoginWithSelfToken(MessageData data, SocketMessageData con)
+    {
         var email = data.GetService<TokenService>().GetEmailFromToken(data.GetAs<string>());
         var user = UserService.Instance.GetUserByEmail(email);
         data.UserId = user.Id;
-        if (data is not SocketMessageData con)
-            return;
         con.Connection.AccountInfo = await SelfUpdatingValue<AccountInfo>.Create(user.Id.ToString(), "accountInfo", () => new());
         var internalToken = data.GetService<TokenService>().CreateToken(email);
         await con.SendBack(data.Create("token", internalToken));
