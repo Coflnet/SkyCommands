68c38c0bbc137a312e0f0c335af32c4cf91d2602
Äkwav <ekwav@coflnet.com>
2023-11-17 22:48:17 +0100

    don't open new context in based on command

--- Commands/Flipper/BasedOnCommand.cs
+++ Commands/Flipper/BasedOnCommand.cs
@@ -1,34 +1,26 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
 
-namespace Coflnet.Sky.Commands
+namespace Coflnet.Sky.Commands;
+public partial class BasedOnCommand : Command
 {
-    public partial class BasedOnCommand : Command
-    {
 
-        public override async Task Execute(MessageData data)
-        {
-            var uuid = data.GetAs<string>();
-            System.Console.WriteLine(uuid);
-            using (var context = new HypixelContext())
-            {
-                var auction = AuctionService.Instance.GetAuction(uuid);
-                if (auction == null)
-                    throw new CoflnetException("auction_unkown", "Auction not found yet, please try again in a few seconds");
-                List<SaveAuction> result = await data.GetService<FlipperService>().GetReferences(uuid);
-                await data.SendBack(data.Create("basedOnResp", result
-                            .Select(a => new BasedOnCommandResponse()
-                            {
-                                uuid = a.Uuid,
-                                highestBid = a.HighestBidAmount,
-                                end = a.End,
-                                ItemName = a.ItemName
-                            }),
-                            A_HOUR));
-            }
-        }
+    public override async Task Execute(MessageData data)
+    {
+        var uuid = data.GetAs<string>();
+        Console.WriteLine(uuid);
+        var auction = AuctionService.Instance.GetAuction(uuid) 
+            ?? throw new CoflnetException("auction_unkown", "Auction not found yet, please try again in a few seconds");
+        List<SaveAuction> result = await data.GetService<FlipperService>().GetReferences(uuid);
+        await data.SendBack(data.Create("basedOnResp", result
+                    .Select(a => new BasedOnCommandResponse()
+                    {
+                        uuid = a.Uuid,
+                        highestBid = a.HighestBidAmount,
+                        end = a.End,
+                        ItemName = a.ItemName
+                    }),
+                    A_HOUR));
     }
 }
