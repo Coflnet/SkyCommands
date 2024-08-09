adcf6315f9a5b1ea60e0a250f1cdb19bbad79a31
Äkwav <ekwav@coflnet.com>
2024-08-09 15:12:12 +0200

    note that flipper is turned off

--- Commands/Flipper/BasedOnCommand.cs
+++ Commands/Flipper/BasedOnCommand.cs
@@ -10,17 +10,26 @@ public partial class BasedOnCommand : Command
     {
         var uuid = data.GetAs<string>();
         Console.WriteLine(uuid);
-        var auction = AuctionService.Instance.GetAuction(uuid) 
+        var auction = AuctionService.Instance.GetAuction(uuid)
             ?? throw new CoflnetException("auction_unkown", "Auction not found yet, please try again in a few seconds");
-        List<SaveAuction> result = await data.GetService<FlipperService>().GetReferences(uuid);
-        await data.SendBack(data.Create("basedOnResp", result
-                    .Select(a => new BasedOnCommandResponse()
-                    {
-                        uuid = a.Uuid,
-                        highestBid = a.HighestBidAmount,
-                        end = a.End,
-                        ItemName = a.ItemName
-                    }),
-                    A_HOUR));
+
+        try
+        {
+
+            List<SaveAuction> result = await data.GetService<FlipperService>().GetReferences(uuid);
+            await data.SendBack(data.Create("basedOnResp", result
+                        .Select(a => new BasedOnCommandResponse()
+                        {
+                            uuid = a.Uuid,
+                            highestBid = a.HighestBidAmount,
+                            end = a.End,
+                            ItemName = a.ItemName
+                        }),
+                        A_HOUR));
+        }
+        catch (System.Exception)
+        {
+            throw new CoflnetException("turned_off", "This feature is currently turned disabled while we are working on getting more servers up and running. Sorry about that, you can use our mod in game to get an estimate of the price.");
+        }
     }
 }
