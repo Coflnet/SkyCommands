99e0bb77d5e510a87d4d7d47c1922b167939c28e
Äkwav <ekwav@coflnet.com>
2024-09-24 19:11:04 +0200

    display message for dupplicate update version

--- Commands/Flipper/UpdateConfigCommand.cs
+++ Commands/Flipper/UpdateConfigCommand.cs
@@ -16,17 +16,20 @@ public class UpdateConfigCommand : SelfDocumentingCommand<ConfigUpdateArgs, Void
         var settings = data.Connection.Settings;
         var key = GetKeyFromname(data.Get().ConfigName);
         using var current = await SelfUpdatingValue<ConfigContainer>.Create(data.UserId.ToString(), key, () => throw new CoflnetException("config_not_found", "The config you are trying to update does not exist"));
-        current.Value.Settings = settings;
-        current.Value.ChangeNotes = data.Get().ChangeNotes;
-        current.Value.Version++;
         var diff = SettingsDiffer.GetDifferences(current.Value.Settings, settings);
         if (diff.GetDiffCount() == 0)
             throw new CoflnetException("no_changes", "No changes found in the config, aborting update");
         if (diff.BlacklistRemoved.Count > settings.BlackList.Count())
             throw new CoflnetException("blacklist_too_large", "More than half of the blacklisted items were removed, aborting update");
-        current.Value.Diffs.Add(current.Value.Version, diff);
+        var newVersion = current.Value.Version + 1;
+        if (current.Value.Diffs.ContainsKey(newVersion))
+            throw new CoflnetException("version_already_exists", "Config already/very recently updated with a new version. Please wait a few seconds and try again");
+        current.Value.Settings = settings;
+        current.Value.ChangeNotes = data.Get().ChangeNotes;
+        current.Value.Version = newVersion;
+        current.Value.Diffs.Add(newVersion, diff);
         await current.Update();
-        await DiHandler.GetService<SettingsService>().UpdateSetting(data.UserId.ToString() + "_archive", key + $"_version_{current.Value.Version}", current.Value);
+        await DiHandler.GetService<SettingsService>().UpdateSetting(data.UserId.ToString() + "_archive", key + $"_version_{newVersion}", current.Value);
         return null;
     }
 
