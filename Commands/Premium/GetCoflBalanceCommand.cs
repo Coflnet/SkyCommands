82ae8251d73167b87840c5b6976c1740568dfd52
Äkwav <ekwav@coflnet.com>
2025-02-03 10:45:00 +0100

    use file scoped namespace

--- Commands/Premium/GetCoflBalanceCommand.cs
+++ Commands/Premium/GetCoflBalanceCommand.cs
@@ -3,19 +3,16 @@ using Coflnet.Sky.Core;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Payments.Client.Api;
 
-namespace Coflnet.Sky.Commands
+namespace Coflnet.Sky.Commands;
+public class GetCoflBalanceCommand : Command
 {
-    public class GetCoflBalanceCommand : Command
+    public override bool Cacheable => false;
+    public override async Task Execute(MessageData data)
     {
-        public override bool Cacheable => false;
-        public override async Task Execute(MessageData data)
-        {
-            var productsApi = DiHandler.GetService<UserApi>();
-            var userData = await productsApi.UserUserIdGetAsync(data.UserId.ToString());
-            if (userData == null)
-                throw new CoflnetException("user_balance_unretrievable", "We had issues loading your balance, if this persists please contact support");
-            await data.SendBack(data.Create("coflBalance", userData.Balance));
-        }
+        var productsApi = DiHandler.GetService<UserApi>();
+        var userData = await productsApi.UserUserIdGetAsync(data.UserId.ToString());
+        if (userData == null)
+            throw new CoflnetException("user_balance_unretrievable", "We had issues loading your balance, if this persists please contact support");
+        await data.SendBack(data.Create("coflBalance", userData.Balance));
     }
-
 }
