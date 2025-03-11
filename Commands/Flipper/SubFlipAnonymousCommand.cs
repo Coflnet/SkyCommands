01785d9d57292f230e2713432dbc26413facc88b
Äkwav <ekwav@coflnet.com>
2025-03-11 18:07:52 +0100

    check again for login and return if now

--- Commands/Flipper/SubFlipAnonymousCommand.cs
+++ Commands/Flipper/SubFlipAnonymousCommand.cs
@@ -2,38 +2,38 @@ using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
 
-namespace Coflnet.Sky.Commands
+namespace Coflnet.Sky.Commands;
+public class SubFlipAnonymousCommand : SubFlipperCommand
 {
-    public class SubFlipAnonymousCommand : SubFlipperCommand
+    public override async Task Execute(MessageData data)
     {
-        public override async Task Execute(MessageData data)
-        {
-            var con = (data as SocketMessageData).Connection;
-            con.OldFallbackSettings?.CancelCompilation();
-            var settings = GetSettings(data);
-            CapListLength(settings.WhiteList, con);
-            CapListLength(settings.BlackList, con);
-            con.OldFallbackSettings = settings;
-            con.SubFlipMsgId = (int)data.mId;
-            data.GetService<FlipperService>().AddNonConnection(con);
-            await data.Ok();
-            await Task.Delay(500); // backof attempt
+        var con = (data as SocketMessageData).Connection;
+        con.OldFallbackSettings?.CancelCompilation();
+        var settings = GetSettings(data);
+        CapListLength(settings.WhiteList, con);
+        CapListLength(settings.BlackList, con);
+        con.OldFallbackSettings = settings;
+        con.SubFlipMsgId = (int)data.mId;
+        data.GetService<FlipperService>().AddNonConnection(con);
+        await data.Ok();
+        await Task.Delay(500); // backof attempt
 
-            static void CapListLength(List<ListEntry> list, SkyblockBackEnd con)
+        static void CapListLength(List<ListEntry> list, SkyblockBackEnd con)
+        {
+            try
             {
-                try
-                {
-                    var id = con.UserId.ToString();
-                }
-                catch (System.Exception)
-                {
-                    return; // actually logged in, don't cap
-                }
+                var id = con.UserId.ToString();
+                return; // actually logged in, don't cap
+            }
+            catch (System.Exception)
+            {
+                // not logged in, cap
                 while (list.Count > 50)
                 {
                     list.RemoveAt(0);
                 }
             }
+
         }
     }
 }
