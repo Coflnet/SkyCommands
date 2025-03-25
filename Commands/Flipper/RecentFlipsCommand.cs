299602092bd33c6aaf4058edcb55b24714e786bf
Äkwav <ekwav@coflnet.com>
2025-03-25 01:32:46 +0100

    reformat RecentflipsCommand

--- Commands/Flipper/RecentFlipsCommand.cs
+++ Commands/Flipper/RecentFlipsCommand.cs
@@ -1,30 +1,26 @@
-using System.Linq;
 using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
 using Newtonsoft.Json;
 
-namespace Coflnet.Sky.Commands
+namespace Coflnet.Sky.Commands;
+public class RecentFlipsCommand : Command
 {
-    public class RecentFlipsCommand : Command
+    public override Task Execute(MessageData data)
     {
-        public override Task Execute(MessageData data)
+        var flipps = data.GetService<FlipperService>()?.Flipps.Take(50);
+        flipps ??= [];
+        try
         {
-            var flipps = data.GetService<FlipperService>()?.Flipps.Take(50);
-            if (flipps == null)
-                flipps = new FlipInstance[0];
-            try
-            {
-                if (data.UserId != 0)
-                    flipps = data.GetService<FlipperService>().Flipps.Skip(50).Take(50);
-                if (data.User.HasPremium)
-                    flipps = data.GetService<FlipperService>().Flipps.Reverse().Skip(2).Take(50);
-            }
-            catch (CoflnetException)
-            {
-                // no premium, continue
-            }
-            return data.SendBack(new MessageData("flips", JsonConvert.SerializeObject(flipps), A_MINUTE));
+            if (data.UserId != 0)
+                flipps = data.GetService<FlipperService>().Flipps.Skip(50).Take(50);
+            if (data.User.HasPremium)
+                flipps = data.GetService<FlipperService>().Flipps.Reverse().Skip(2).Take(50);
         }
+        catch (CoflnetException)
+        {
+            // no premium, continue
+        }
+        return data.SendBack(new MessageData("flips", JsonConvert.SerializeObject(flipps), A_MINUTE));
     }
 }
