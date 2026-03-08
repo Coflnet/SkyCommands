7f51150d2552ff770c8fc52c40cdf59a267573d6
Äkwav <ekwav@coflnet.com>
2026-03-08 14:56:33 +0100

    change flip settings to file scoped

--- Commands/Flipper/GetFlipSettingsCommand.cs
+++ Commands/Flipper/GetFlipSettingsCommand.cs
@@ -2,28 +2,27 @@ using System.Threading.Tasks;
 using Coflnet.Sky.Commands.Shared;
 using Coflnet.Sky.Core;
 
-namespace Coflnet.Sky.Commands
-{
-    public class GetFlipSettingsCommand : Command
-    {
-        public override async Task Execute(MessageData data)
-        {
-            var con = (data as SocketMessageData).Connection;
-            await AssignSettings(con);
-            await data.SendBack(data.Create("flipSettings", con.FlipSettings.Value));
-        }
+namespace Coflnet.Sky.Commands;
 
-        public static async Task AssignSettings(SkyblockBackEnd con)
+public class GetFlipSettingsCommand : Command
+{
+    public override async Task Execute(MessageData data)
+    {
+        var con = (data as SocketMessageData).Connection;
+        await AssignSettings(con);
+        await data.SendBack(data.Create("flipSettings", con.FlipSettings.Value));
+    }
+
+    public static async Task AssignSettings(SkyblockBackEnd con)
+    {
+        var userId = con.UserId;
+        con.FlipSettings = await SelfUpdatingValue<FlipSettings>.Create(userId.ToString(), "flipSettings", () => new FlipSettings()
         {
-            var userId = con.UserId;
-            con.FlipSettings = await SelfUpdatingValue<FlipSettings>.Create(userId.ToString(), "flipSettings", () => new FlipSettings()
-            {
-                MinProfit = 100000,
-                MinVolume = 2,
-                ModSettings = new ModSettings() { ShortNumbers = true },
-                Visibility = new VisibilitySettings() { SellerOpenButton = true, ExtraInfoMax = 3, Lore = true }
-            });
-            con.FlipSettings.Value.PlayerInfo = con;
-        }
+            MinProfit = 100000,
+            MinVolume = 2,
+            ModSettings = new ModSettings() { ShortNumbers = true },
+            Visibility = new VisibilitySettings() { SellerOpenButton = true, ExtraInfoMax = 3, Lore = true }
+        });
+        con.FlipSettings.Value.PlayerInfo = con;
     }
 }
